4aad2f0de2dd372102c6214a012cb7f1fd3b7711
wedoenhet55 <94610459+wedoenhet55@users.noreply.github.com>
2022-03-25 14:16:06 +0100

    done

---
+++ xjava.js
@@ -0,0 +1,47 @@
+
+//hier mijn javascript voor image slides
+
+const dots = document.querySelectorAll(".dot-container button");
+const images = document.querySelectorAll(".image-container img");
+
+let i = 0; // current slide
+let j = 4; // total slides
+
+function next(){
+    document.getElementById("content" + (i+1)).classList.remove("active");
+    i = ( j + i + 1) % j;
+    document.getElementById("content" + (i+1)).classList.add("active");
+    indicator( i+ 1 );
+}
+
+function prev(){
+    document.getElementById("content" + (i+1)).classList.remove("active");
+    i = (j + i - 1) % j;
+    document.getElementById("content" + (i+1)).classList.add("active");
+    indicator(i+1);
+}
+
+function indicator(num){
+    dots.forEach(function(dot){
+        dot.style.backgroundColor = "transparent";
+    });
+    document.querySelector(".dot-container button:nth-child(" + num + ")").style.backgroundColor = "#076bb8";
+}
+
+function dot(index){
+    images.forEach(function(image){
+        image.classList.remove("active");
+    });
+    document.getElementById("content" + index).classList.add("active");
+    i = index - 1;
+    indicator(index);
+}
+
+
+const previousButton = document.querySelector('.previous-button');
+if (previousButton) {
+    previousButton.addEventListener('click', prev);
+}
+
+console.log('loaded');
+
